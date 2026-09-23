10edf2813832c02cd7fef5e1fdcdece78660fda4
Gauravkushwaha2007 <gaurav121212kush@gmail.com>
2026-09-23 15:24:02 +0530

    Database and Server setup done

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+async function connectToDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to DB');
+    } catch (error) {
+        console.log('Error while connecting to DB:', error);
+    }
+}
+
+connectToDB();
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+});
